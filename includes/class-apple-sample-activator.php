a117db7ef78404ddaf25e67609fc966fd4e1390f
Pravin R <pravin@coderzvisiontech.com>
2022-04-08 11:08:50 +0530

    added files

---
+++ includes/class-apple-sample-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       test.com
+ * @since      1.0.0
+ *
+ * @package    Apple_Sample
+ * @subpackage Apple_Sample/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Apple_Sample
+ * @subpackage Apple_Sample/includes
+ * @author     test <test@mail.com>
+ */
+class Apple_Sample_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-apple-sample-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       test.com
+ * @since      1.0.0
+ *
+ * @package    Apple_Sample
+ * @subpackage Apple_Sample/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Apple_Sample
+ * @subpackage Apple_Sample/includes
+ * @author     test <test@mail.com>
+ */
+class Apple_Sample_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
